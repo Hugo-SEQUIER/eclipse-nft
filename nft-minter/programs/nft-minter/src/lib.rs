a67e852d3eabb1af39d76be478dc62073dcd7481
Hugo Sequier <hugosequier@gmail.com>
2024-09-11 15:38:25 +0300

    error solved ?

--- nft-minter/programs/nft-minter/src/lib.rs
+++ nft-minter/programs/nft-minter/src/lib.rs
@@ -1,7 +1,8 @@
 use anchor_lang::prelude::*;
 use anchor_spl::token::{self, Token, TokenAccount, Mint};
 use anchor_spl::associated_token::AssociatedToken;
-use mpl_token_metadata::instruction::{create_metadata_accounts_v3, create_master_edition_v3};
+use mpl_token_metadata::state::{Creator, DataV2};
+use mpl_token_metadata::instructions::{CreateMetadataAccountsV3, CreateMasterEditionV3};
 
 declare_id!("DNxFQyTTC6k1HBHfcuGEhP28eT94aoRwjxT4o4TNbBkR");
 
@@ -27,73 +28,60 @@ pub mod nft_minter {
         token::mint_to(cpi_context, 1)?;
 
         // Create metadata account
-        let accounts = mpl_token_metadata::accounts::CreateMetadataAccountsV3 {
-            metadata: ctx.accounts.metadata.to_account_info(),
-            mint: ctx.accounts.mint.to_account_info(),
-            mint_authority: ctx.accounts.payer.to_account_info(),
-            payer: ctx.accounts.payer.to_account_info(),
-            update_authority: ctx.accounts.payer.to_account_info(),
-            system_program: ctx.accounts.system_program.to_account_info(),
-            rent: ctx.accounts.rent.to_account_info(),
-        };
-        let creator = vec![mpl_token_metadata::state::Creator {
+        let creator = vec![Creator {
             address: ctx.accounts.payer.key(),
             verified: false,
             share: 100,
         }];
-        let instruction = create_metadata_accounts_v3(
-            ctx.accounts.token_metadata_program.key(),
-            accounts.metadata.key(),
-            accounts.mint.key(),
-            accounts.mint_authority.key(),
-            accounts.payer.key(),
-            accounts.update_authority.key(),
+
+        let data_v2 = DataV2 {
             name,
             symbol,
             uri,
-            Some(creator),
-            1,
-            true,
-            false,
-            None,
-            None,
-            None,
+            seller_fee_basis_points: 0,
+            creators: Some(creator),
+            collection: None,
+            uses: None,
+        };
+
+        let cpi_context = CpiContext::new(
+            ctx.accounts.token_metadata_program.to_account_info(),
+            CreateMetadataAccountsV3 {
+                metadata: ctx.accounts.metadata.to_account_info(),
+                mint: ctx.accounts.mint.to_account_info(),
+                mint_authority: ctx.accounts.payer.to_account_info(),
+                payer: ctx.accounts.payer.to_account_info(),
+                update_authority: ctx.accounts.payer.to_account_info(),
+                system_program: ctx.accounts.system_program.to_account_info(),
+                rent: ctx.accounts.rent.to_account_info(),
+            },
         );
-        anchor_lang::solana_program::program::invoke(
-            &instruction,
-            &[
-                ctx.accounts.metadata.to_account_info(),
-                ctx.accounts.mint.to_account_info(),
-                ctx.accounts.payer.to_account_info(),
-                ctx.accounts.token_metadata_program.to_account_info(),
-                ctx.accounts.token_program.to_account_info(),
-                ctx.accounts.system_program.to_account_info(),
-                ctx.accounts.rent.to_account_info(),
-            ],
+
+        mpl_token_metadata::instructions::create_metadata_accounts_v3(
+            cpi_context,
+            data_v2,
+            true,
+            true,
+            None,
         )?;
 
         // Create master edition account
-        let master_edition_infos = vec![
-            ctx.accounts.master_edition.to_account_info(),
-            ctx.accounts.mint.to_account_info(),
-            ctx.accounts.payer.to_account_info(),
-            ctx.accounts.metadata.to_account_info(),
+        let cpi_context = CpiContext::new(
             ctx.accounts.token_metadata_program.to_account_info(),
-            ctx.accounts.token_program.to_account_info(),
-            ctx.accounts.system_program.to_account_info(),
-            ctx.accounts.rent.to_account_info(),
-        ];
-        let master_edition_instruction = create_master_edition_v3(
-            ctx.accounts.token_metadata_program.key(),
-            ctx.accounts.master_edition.key(),
-            ctx.accounts.mint.key(),
-            ctx.accounts.payer.key(),
-            ctx.accounts.payer.key(),
-            ctx.accounts.metadata.key(),
-            ctx.accounts.payer.key(),
-            Some(0),
+            CreateMasterEditionV3 {
+                edition: ctx.accounts.master_edition.to_account_info(),
+                mint: ctx.accounts.mint.to_account_info(),
+                update_authority: ctx.accounts.payer.to_account_info(),
+                mint_authority: ctx.accounts.payer.to_account_info(),
+                payer: ctx.accounts.payer.to_account_info(),
+                metadata: ctx.accounts.metadata.to_account_info(),
+                token_program: ctx.accounts.token_program.to_account_info(),
+                system_program: ctx.accounts.system_program.to_account_info(),
+                rent: ctx.accounts.rent.to_account_info(),
+            },
         );
-        anchor_lang::solana_program::program::invoke(&master_edition_instruction, &master_edition_infos)?;
+
+        mpl_token_metadata::instructions::create_master_edition_v3(cpi_context, Some(0))?;
 
         Ok(())
     }
@@ -110,15 +98,18 @@ pub struct MintNFT<'info> {
         associated_token::authority = payer
     )]
     pub token_account: Account<'info, TokenAccount>,
+    /// CHECK: This is not dangerous because we don't read or write from this account
     #[account(mut)]
-    pub metadata: AccountInfo<'info>,
+    pub metadata: UncheckedAccount<'info>,
+    /// CHECK: This is not dangerous because we don't read or write from this account
     #[account(mut)]
-    pub master_edition: AccountInfo<'info>,
+    pub master_edition: UncheckedAccount<'info>,
     #[account(mut)]
     pub payer: Signer<'info>,
     pub rent: Sysvar<'info, Rent>,
     pub system_program: Program<'info, System>,
     pub token_program: Program<'info, Token>,
     pub associated_token_program: Program<'info, AssociatedToken>,
-    pub token_metadata_program: AccountInfo<'info>,
+    /// CHECK: This is not dangerous because we don't read or write from this account
+    pub token_metadata_program: UncheckedAccount<'info>,
 }
